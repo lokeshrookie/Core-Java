9a1ab1801a789c0fcdd132a5ec0a59fed797cc9f
Lokesh <rocklinglokesh@gmail.com>
2021-10-11 13:35:34 +0530

    initial commit

--- src/com/Lokesh/Array_Practice2.java
+++ src/com/Lokesh/Array_Practice2.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 class Array_practice2
 {
-
     public static void main(String[] args) {
         System.out.println("Hello World");
         Scanner s = new Scanner(System.in);
@@ -13,6 +12,7 @@ class Array_practice2
         //lets take a keyword from the user.
         System.out.println("Enter a String :");
         String k = s.next();
+        s.close();
         boolean b = Arrays.asList(keywords).contains(k); //asList is a method in Arrays class is used to change the array into a list
         //then .contains method is used to check whether the passed value is present in the given string or not.
         if(b == true )
