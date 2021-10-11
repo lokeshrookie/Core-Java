2baf099694f54e1e54a6ea624fb33375f3272060
Lokesh <rocklinglokesh@gmail.com>
2021-10-11 13:34:01 +0530

    initial commit

--- src/com/Lokesh/Strings/Intro.java
+++ src/com/Lokesh/Strings/Intro.java
@@ -7,6 +7,7 @@ public class Intro {
         Scanner s = new Scanner(System.in);
         String A = s.next();
         String B = s.next();
+        s.close();
         System.out.println(A.length()+ B.length());
         System.out.println(B.compareTo(A));
         System.out.println(A.toUpperCase() + " " +  B.toUpperCase());
