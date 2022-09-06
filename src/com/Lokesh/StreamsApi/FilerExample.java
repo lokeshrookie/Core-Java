8bcd53339981d0fe3e197bdb79e718cb5df8e7d1
Lokesh <rocklinglokesh@gmail.com>
2022-09-06 15:57:33 +0530

    Streams

--- src/com/Lokesh/StreamsApi/FilerExample.java
+++ src/com/Lokesh/StreamsApi/FilerExample.java
@@ -1,8 +1,7 @@
 package com.Lokesh.StreamsApi;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FilerExample {
@@ -14,14 +13,39 @@ public class FilerExample {
         students.add(new Student("Rakesh",97));
         students.add(new Student("Prudhvi", 94));
 
+
+
+
         // Printing all the Students
         students.forEach(person -> System.out.println(person.name + " "  + person.marks));
+
 
         // Returns the collection of toppers as a List
         List<Student> topperClub = students.stream().filter(student -> student.marks > 95).collect(Collectors.toList());
 
         // Printing Toppers
         topperClub.forEach(topper -> System.out.println(topper.name));
+
+
+        // Sorting Students based on Name.
+        List<Student> listStudents = students.stream().sorted(Comparator.comparing(student -> student.name)).collect(Collectors.toList());
+
+        // Sorting Students based on Marks.
+        List<Student> listMarks = students.stream().sorted(Comparator.comparing(student -> student.marks)).collect(Collectors.toList());
+
+
+        // Print Sorted Students (based on marks) list.
+        listMarks.forEach(student -> System.out.println(student.marks));
+        System.out.println("Student with max marks: ");
+
+//        listMarks.stream().max(Comparator.comparing(student -> student.name))
+//                .ifPresent(System.out::println);
+        // The above statement is used to directly print the value if present.
+
+        // This statement is used to store the value in max variable if present.
+        // to check if present,  call max.isPresent().
+        // print max directly to know if it is present or not
+        Optional<Student> max =listMarks.stream().max(Comparator.comparing(student -> student.marks==100));
 
 
     }
@@ -35,5 +59,14 @@ class Student{
         this.name = name;
         this.marks = marks;
     }
-
+    public   void hello(){
+        System.out.println("hello");
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "marks=" + marks +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
